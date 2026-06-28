fb9c741b28fcf38a6814049fdf43f2140eca5de8
Laureano Sierra <laureanosierra.dev@gmail.com>
2026-06-28 16:49:29 -0300

    fix: stripe api version actualizada a 2026-06-24.dahlia

--- pages/api/stripe-webhook.ts
+++ pages/api/stripe-webhook.ts
@@ -63,7 +63,7 @@ export default async function handler(
   }
 
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-    apiVersion: "2025-04-30.basil",
+    apiVersion: "2026-06-24.dahlia",
   });
 
   const rawBody = await getRawBody(req);
